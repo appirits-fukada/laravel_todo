826a0079f95566e83f6b8788c9e0447d5370eb13
kohei.fukada <kohei.fukada@appirits.com>
2022-08-29 17:30:42 +0900

    ルーティングとビューの追加、コントローラーの修正

--- src/tests/Feature/TaskControllerTest.php
+++ src/tests/Feature/TaskControllerTest.php
@@ -5,21 +5,22 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Task;
 
 class TaskControllerTest extends TestCase
 {
     /** @test */
     public function タスク一覧にアクセスして画面が表示される()
     {
-        $response = $this->get(action('TaskController@index'));
+        $response = $this->get(action('App\Http\Controllers\TaskController@index'));
         $response->assertStatus(200);
     }
 
     /** @test */
     public function タスク一覧にアクセスしてタスク一覧画面が表示される()
     {
-        $response = $this->get(action('TaskController@index'));
-        $response->assertViewIs('layouts.index');
+        $response = $this->get(action('App\Http\Controllers\TaskController@index'));
+        $response->assertViewIs('tasks.index');
     }
 
     /** @test */
@@ -29,8 +30,11 @@ class TaskControllerTest extends TestCase
         Task::factory()->count(30)->create();
 
         // 作成したデータが全件表示されていること
-        $response = $this->get(action('TaskController@index'));
-        $task = $response->original['tasks'];
+        //TODO ①テスト用DBが分けられてない。
+        //TODO ②上記の"テスト用データを作成"では作成したテストデータがテスト後ロールバックされない。
+        // ①②から、テスト実行のたびに開発用DBにレコードが溜まっちゃって30件以上あるのでREDになる。
+        $response = $this->get(action('App\Http\Controllers\TaskController@index'));
+        $tasks = $response->original['tasks'];
         $this->assertEquals(30, count($tasks));
     }
 }
